2d247646df68c007e0513c3c96ad3c48f5445ae0
Michele Bisceglia <mikael_bisceglia@libero.it>
2022-06-09 17:00:06 +0200

    stampa in pagina

--- index.php
+++ index.php
@@ -23,13 +23,6 @@ $text= [
                     Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione."
     ],
     [
-        "question" => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
-        "answer" => "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.
-                    Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.
-                    Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.
-                    Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo."
-    ],
-    [
         "question" => "Perché il mio account è associato a un paese?",
         "answer" => "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
                     La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
@@ -63,8 +56,9 @@ $text= [
       <title>Document</title>
     </head>
     <body>
-      <h2>
-        
-      </h2>
+      <?php 
+      foreach($text as $item) {
+      echo "<h3>" . $item["question"] . "</h3>" . " " . "<p>" . $item["answer"]; "</p>" ;
+    } ?>
     </body>
     </html>
